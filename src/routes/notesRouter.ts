205ca6686987c7e9561ac8683881c3ff1acc1930
Brent Rucker <brent.rucker@gmail.com>
2023-05-04 19:58:47 -0700

    error handling

--- src/routes/notesRouter.ts
+++ src/routes/notesRouter.ts
@@ -14,10 +14,13 @@ app.use(express.json());
 app.get('/notes', async (req: Request, res: Response) => {
   try {
     const noteResponse: NoteResponse[] = await getAllNotes();
+    if (!noteResponse) {
+      res.status(404).send("Error: There was an error getting notes");
+    }
     res.status(200).json(noteResponse);
   }
   catch(error) {
-    res.send("Error: "+error);
+    res.status(500).send("Error getting notes: "+error);
   }
 });
 
@@ -30,11 +33,17 @@ app.get('/notes', async (req: Request, res: Response) => {
 app.get('/note/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+    if (!id) {
+      res.status(400).send("Error: No id was given");
+    }
     const noteResponse: NoteResponse[] = await getNoteById(+id);
+    if (!noteResponse) {
+      res.status(404).send(`Error: No Note was found with id ${id}`);
+    }
     res.status(200).json(noteResponse);
   }
   catch(error) {
-    res.send("Error: "+error);
+    res.status(500).send("Error getting note by id: "+error);
   }
 });
 
@@ -46,19 +55,26 @@ app.get('/note/:id', async (req: Request, res: Response) => {
  */
 app.post('/note', async (req: Request, res: Response) => {
   try {
+    const { first_name, last_name, user_permission, team_id,note, rating } = req.body;
+    if (!first_name || !last_name || !user_permission || !team_id || !note || !rating) {
+      res.status(400).send("Error: The information to create a note is not valid");
+    }
     const noteRequest: Note = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      user_permission: req.body.user_permission,
-      team_id: req.body.team_id,
-      note: req.body.note,
-      rating: req.body.rating
+      first_name,
+      last_name,
+      user_permission,
+      team_id,
+      note,
+      rating
     }
     const postNoteResponseId: idResponse[] = await createNote(noteRequest);
+    if (!postNoteResponseId) {
+      res.status(404).send("Error: There was an error creating a note");
+    }
     res.status(200).json(postNoteResponseId);
   }
   catch(error) {
-    res.send("Error: "+error);
+    res.status(500).send("Error creating note: "+error);
   }
 });
 
@@ -71,12 +87,21 @@ app.post('/note', async (req: Request, res: Response) => {
 app.put('/note/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+    if (!id) {
+      res.status(400).send("Error: No id was given");
+    }
     const { note, rating } = req.body;
+    if (!note || !rating) {
+      res.status(400).send("Error: note or rating was not given");
+    }
     const noteResponseId: idResponse[] = await updateNoteById(+id, note, +rating);
+    if (!noteResponseId) {
+      res.status(404).send(`Error: There was an error updating note with id ${id}`);
+    }
     res.status(200).json(noteResponseId);
   }
   catch(error) {
-    res.send("Error: "+error);
+    res.status(500).send("Error updating note: "+error);
   }
 });
 
